56229703748317e7cdd816f7ba4dfb9bb8e6ad0b
helluvalazyass <monuuyad@gmail.com>
2019-05-22 07:49:25 +0530

    initial commit

---
+++ src/Components/item.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function item (props) {
+  return (
+    <div>
+      <img src="" alt="item image"></img>
+      <h3>
+        item title
+      </h3>
+      <p>
+        item description
+      </p>
+    </div>
+  )
+}
