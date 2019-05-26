bd3bd080896defabcd2b486c9433ed90cc9aee84
helluvalazyass <monuuyad@gmail.com>
2019-05-26 16:17:43 +0530

    added Item card in home and added default image

--- src/Components/App.js
+++ src/Components/App.js
@@ -9,12 +9,56 @@ import {
   Link,
 } from 'react-router-dom'
 import './App.css';
+import ItemPlaceholderImage from '../Images/image-placeholder.png'
 
+
+function Loading () {
+  return (
+    <div>Loading</div>
+  )
+}
 class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      items: [],
+      items: [
+        {
+          name: 'Item1',
+          description: 'This is a short item description. It describes the item.',
+          price: 2000,
+          img_url: ItemPlaceholderImage,
+        },
+        {
+          name: 'Item2',
+          description: 'This is an item description. It describes the item. Describe means explaining the about the item.',
+          price: 2000,
+          img_url: ItemPlaceholderImage,
+        },
+        {
+          name: 'Item3',
+          description: 'This is an item description. It describes the item. Describe means explaining the about the item.',
+          price: 2000,
+          img_url: ItemPlaceholderImage,
+        },
+        {
+          name: 'Item4',
+          description: 'This is an item description. It describes the item. Describe means explaining the about the item.',
+          price: 2000,
+          img_url: ItemPlaceholderImage,
+        },
+        {
+          name: 'Item5',
+          description: 'This is an item description. It describes the item. Describe means explaining the about the item.',
+          price: 2000,
+          img_url: ItemPlaceholderImage,
+        },
+        {
+          name: 'Item6',
+          description: 'This is an item description. It describes the item. Describe means explaining the about the item.',
+          price: 2000,
+          img_url: ItemPlaceholderImage,
+        },
+      ],
       loading: true,
     }
   }
@@ -25,7 +69,17 @@ class App extends Component {
         <div >
           <Nav />
           <hr />
-          <Route exact path='/' component={Home} />
+          <Route exact path='/' render={() => {
+            // if(this.state.loading) {
+            //   return <Loading />
+            // }
+            // else {
+              return (
+                <Home items={this.state.items} />
+              )
+            // }
+            
+          }} />
           <Route path='/about' component={About} />
           <Route path='/my-account' component={MyAccount} />
         </div>
