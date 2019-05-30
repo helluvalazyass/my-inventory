6b004b29123618f748682bd5137a6f3d03c3d5e4
helluvalazyass <monuuyad@gmail.com>
2019-05-30 19:10:30 +0530

    created add Item Form

--- src/Components/AddItemForm.js
+++ src/Components/AddItemForm.js
@@ -1,9 +1,88 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function AddItemForm () {
-  return (
-    <div>
-      AddItemForm
-    </div>
-  )
+export default class AddItemForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      itemNameInput:'',
+      itemDescriptionInput: '',
+      itemImageInput: '',
+    }
+    this.handleResetForm = this.handleResetForm.bind(this)
+    this.handleSubmitForm = this.handleSubmitForm.bind(this)
+  }
+  
+  handleResetForm() {
+    this.setState({
+      itemNameInput:'',
+      itemDescriptionInput: '',
+      itemImageInput: '',
+    })
+  }
+
+  handleItemNameInput(e) {
+    this.setState({
+      itemNameInput: e.target.value,
+    })
+  }
+
+  handleItemDescriptionInput(e) {
+    this.setState({
+      itemDescriptionInput: e.target.value,
+    })
+  }
+
+  handleItemImageInput(e) {
+    this.setState({
+      itemImageInput: e.target.value,
+    })
+  }
+
+  handleSubmitForm(e) {
+    e.preventDefault()
+  }
+
+  render(){
+    return (
+      <div className='d-flex justify-content-center align-items-center'>
+        <form id='addItemForm' className='w-50'>
+          <div className='form-group row'>
+            <label for='itemNameInput' className='col-sm-4 col-form-label'> Item Name </label>
+            <input 
+            className='form-control col-sm-8' 
+            id='itemNameInput' 
+            type='text' 
+            placeholder='Enter Item Name' 
+            value={this.state.itemNameInput}
+            onChange={(e) => this.handleItemNameInput(e)}  />
+          </div>
+          <div className='form-group row'>
+            <label for='itemDescriptionInput' className='col-sm-4 col-form-label'> Item Description </label>
+            <textarea 
+            className='form-control col-sm-8' 
+            id='itemDescriptionInput' 
+            placeholder='Enter Item Description' 
+            value={this.state.itemDescriptionInput} 
+            row='4'
+            onChange={(e) => this.handleItemDescriptionInput(e)}  />
+          </div>
+          <div className='form-group row'>
+            <label for='itemImageInput' className='col-sm-4 col-form-label'>Item Image</label>
+            <input 
+            id='itemImageInput' 
+            type='file' 
+            className='col-sm-8 form-control-file' 
+            value={this.state.itemImageInput}
+            onChange={(e) => this.handleItemImageInput(e)}  />
+            <small className='form-text text-muted'>Only upload .jpg or .png files</small>
+          </div>
+          <div className='mt-5 d-flex justify-content-end'>
+          <button className='btn btn-secondary ml-3' type='reset' onClick={this.handleResetForm}>Reset</button>
+          <button className='btn btn-success ml-3' type='submit' onClick={this.handleSubmitForm}>Add Item</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+ 
 }
--- src/Components/MyAccount.js
+++ src/Components/MyAccount.js
@@ -1,36 +1,58 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ItemList from './ItemList'
 import AddItemForm from './AddItemForm'
 import {
   Route,
   Link,
   withRouter,
+  Redirect,
 } from 'react-router-dom'
 
 
-function MyAccount (props) {
-  return (
-    <div>
-      <aside>
-        <ul className='nav nav-pills flex-column'>
-          <li className="nav-item">
-            <Link className="nav-link" to={`${props.match.url}/add-item`}>Add Item</Link>
-          </li>
-          <li>
-            <Link className="nav-link" to={`${props.match.url}/delete-item`}>Delete Item</Link>
-          </li>
-        </ul>
-      </aside>
-      <div>
-        <Route path={`${props.match.path}/add-item`} component={AddItemForm} />
-        <Route path={`${props.match.path}/delete-item`} render={() => {
-          return (
-            <ItemList items={props.items} onDeleteItem={props.onDeleteItem} />
-          )
-        }} />
+class MyAccount extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      activeMenu: 'add',
+    }
+  }
+
+  // handleActiveMenu(menu) {
+  //   this.setState = {
+  //     activeMenu: menu,
+  //   }
+  // }
+
+  render() {
+    return (
+      <div className='row'>
+        <aside className='col-sm-2'>
+          <ul className='nav nav-pills flex-column'>
+            <li className="nav-item">
+              <Link className={`nav-link ${this.state.activeMenu === 'add'? 'active':''}`} to={`${this.props.match.url}/add-item`} onClick={() => {
+                this.setState({activeMenu: 'add'})
+              }}>Add Item</Link>
+            </li>
+            <li>
+              <Link className={`nav-link ${this.state.activeMenu === 'delete'? 'active':''}`} to={`${this.props.match.url}/delete-item`} onClick={() => {
+                this.setState({activeMenu: 'delete'})
+              }} >Delete Item</Link>
+            </li>
+          </ul>
+        </aside>
+        <div className='col-sm-10'>
+          <Route exact path={`${this.props.match.path}`} render={() => <Redirect to={`${this.props.match.path}/add-item`}/>} />
+          <Route path={`${this.props.match.path}/add-item`} component={AddItemForm} />
+          <Route path={`${this.props.match.path}/delete-item`} render={() => {
+            return (
+              <ItemList items={this.props.items} onDeleteItem={this.props.onDeleteItem} />
+            )
+          }} />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+   
 }
 
 export default withRouter(MyAccount)
